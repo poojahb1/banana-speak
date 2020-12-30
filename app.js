0d3942efd49708a7cdd3c1714bb244fc2e25f7fa
poojahb1 <poojaamaaheshwaari@gmail.com>
2020-12-30 10:51:49 +0530

    2 files changed

---
+++ app.js
@@ -0,0 +1,9 @@
+// console.log("hello there! script is working from diff file");
+// var username = prompt("give me your username");
+// alert("this script works!!" + username);
+
+var btntranslate = document.querySelector("#btn-translate");
+// console.log(btntranslate);
+btntranslate.addEventListener("click", function clickEventHandler(){
+  console.log("clicked");
+})
