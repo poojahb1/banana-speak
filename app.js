372053a68603f5c5f3bd5d88bb56344953005824
poojahb1 <poojaamaaheshwaari@gmail.com>
2020-12-30 13:10:15 +0530

    2 files changed

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 // console.log("hello there! script is working from diff file");
 // var username = prompt("give me your username");
 // alert("this script works!!" + username);
+// "input[name='translator']"  -->  input element with attribute name='traslator'
 
 var btntranslate = document.querySelector("#btn-translate");
+var textinput = document.querySelector("#text-input");
+var outputdiv = document.querySelector("#output");
+// outputdiv.innerText = "pooja maheshwari";
 // console.log(btntranslate);
+// console.log(textinput);
+// console.log(outputdiv);
+
 btntranslate.addEventListener("click", function clickEventHandler(){
-  console.log("clicked");
-  console.log("input", textinput.value);
+  // console.log("clicked");
+  // console.log("input", textinput.value);
+  outputdiv.innerText = "hefgeur gyegfyh gewgh " + textinput.value;
 })
 
-var textinput = document.querySelector("#text-input");
-console.log(textinput);
-
-// "input[name='translator']"  -->  input element with attribute name='traslator'
 
